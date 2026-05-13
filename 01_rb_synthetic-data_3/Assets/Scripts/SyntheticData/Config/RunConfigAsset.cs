197b627c3c2153d51e0c1cc0737a7b8e1a336f8a
Mitchell Quinn <mitch@starcroft.net>
2026-05-13 16:52:17 +0100

    Configure validation run and Defender materials
    - Add a custom inspector for RunConfigAsset serialized config editing
    - Switch RunConfigAsset to v28 validation settings with 60k samples- Add black and blue Defender materials and assign them in the scene/model importer

--- 01_rb_synthetic-data_3/Assets/Scripts/SyntheticData/Config/RunConfigAsset.cs
+++ 01_rb_synthetic-data_3/Assets/Scripts/SyntheticData/Config/RunConfigAsset.cs
@@ -1,6 +1,10 @@
 using RaccoonBall.SyntheticData.Core;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace RaccoonBall.SyntheticData.Config
 {
     [CreateAssetMenu(menuName = "Raccoon Ball/Synthetic Data/Run Config Asset", fileName = "RunConfigAsset")]
@@ -14,4 +18,33 @@ namespace RaccoonBall.SyntheticData.Config
             return JsonUtility.FromJson<RunConfig>(json);
         }
     }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(RunConfigAsset))]
+    internal sealed class RunConfigAssetEditor : Editor
+    {
+        private SerializedProperty _configProperty;
+
+        private void OnEnable()
+        {
+            _configProperty = serializedObject.FindProperty("_config");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            if (_configProperty == null)
+            {
+                EditorGUILayout.HelpBox("Serialized field '_config' was not found. Reimport scripts and reopen this asset.", MessageType.Warning);
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(_configProperty, includeChildren: true);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+#endif
 }
